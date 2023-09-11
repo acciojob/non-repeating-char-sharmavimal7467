30234a52646f17d9bc1668086786007ec2491901
sharmavimal7467 <139248526+sharmavimal7467@users.noreply.github.com>
2023-09-11 15:29:25 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,14 @@
 function firstNonRepeatedChar(str) {
  // Write your code here
+	let charMap = {};
+	for(let char of str){
+		charMap[char] = (charMap[char]||0)+1;
+	}
+		for(let i = 0;i<str.length;i++){
+			if(charMap[str[i]]===1){
+				return str[i];
+			}
+		}
 }
 const input = prompt("Enter a string");
 alert(firstNonRepeatedChar(input)); 
